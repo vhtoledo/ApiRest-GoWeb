20fc2f4c5dcd018c299089ab5e804123b90c079b
victor toledo <querynetworking@gmail.com>
2023-08-18 22:15:05 -0300

    ApiRest Con Go - Mysql - orm(Grom)

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,84 +1,90 @@
 package handlers
 
 import (
-	"gorm/models"
 	"encoding/json"
+	"gorm/db"
+	"gorm/models"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
 
 func GetUsers(rw http.ResponseWriter, r *http.Request) {
 
-	if users, err := models.ListUsers(); err != nil {
-		models.SendNotFound(rw)
-	} else {
-		models.SendData(rw, users)
-	}
+	users := models.Users{}
+	db.Database.Find(&users)
+	sendData(rw, users, http.StatusOK)
 
 }
 
 func GetUser(rw http.ResponseWriter, r *http.Request) {
-
-	if user, err := getUserByRequest(r); err != nil {
-		models.SendNotFound(rw)
+	if user, err := getUserById(r); err != nil {
+		sendError(rw, http.StatusNotFound)
 	} else {
-		models.SendData(rw, user)
+		sendData(rw, user, http.StatusOK)
+	}
+
+}
+
+func getUserById(r *http.Request) (models.User, *gorm.DB) {
+	//Obtener ID
+	vars := mux.Vars(r)
+	userId, _ := strconv.Atoi(vars["id"])
+	user := models.User{}
+
+	if err := db.Database.First(&user, userId); err.Error != nil {
+		return user, err
+	} else {
+		return user, nil
 	}
 }
 
 func CreateUser(rw http.ResponseWriter, r *http.Request) {
 
+	//Obtener registro
 	user := models.User{}
 	decoder := json.NewDecoder(r.Body)
 
 	if err := decoder.Decode(&user); err != nil {
-		models.SendUnprocessableEntity(rw)
+		sendError(rw, http.StatusUnprocessableEntity)
 	} else {
-		user.Save()
-		models.SendData(rw, user)
+		db.Database.Save(&user)
+		sendData(rw, user, http.StatusCreated)
 	}
 }
+
 func UpdateUser(rw http.ResponseWriter, r *http.Request) {
-	//Obtener ID
+
+	//Obtener registro
 	var userId int64
-	if user, err := getUserByRequest(r); err != nil {
-		models.SendNotFound(rw)
-	} else {
-		userId = user.Id
-	}
 
-	user := models.User{}
-	decoder := json.NewDecoder(r.Body)
-
-	if err := decoder.Decode(&user); err != nil {
-		models.SendUnprocessableEntity(rw)
+	if user_ant, err := getUserById(r); err != nil {
+		sendError(rw, http.StatusNotFound)
 	} else {
-		user.Id = userId
-		user.Save()
-		models.SendData(rw, user)
+		userId = user_ant.Id
+
+		user := models.User{}
+		decoder := json.NewDecoder(r.Body)
+
+		if err := decoder.Decode(&user); err != nil {
+			sendError(rw, http.StatusUnprocessableEntity)
+		} else {
+			user.Id = userId
+			db.Database.Save(&user)
+			sendData(rw, user, http.StatusOK)
+		}
 	}
 
 }
+
 func DeleteUser(rw http.ResponseWriter, r *http.Request) {
 
-	if user, err := getUserByRequest(r); err != nil {
-		models.SendNotFound(rw)
+	if user, err := getUserById(r); err != nil {
+		sendError(rw, http.StatusNotFound)
 	} else {
-		user.Delete()
-		models.SendData(rw, user)
-	}
-}
-
-func getUserByRequest(r *http.Request) (models.User, error) {
-	//Obtener ID
-	vars := mux.Vars(r)
-	userId, _ := strconv.Atoi(vars["id"])
-
-	if user, err := models.GetUser(userId); err != nil {
-		return *user, err
-	} else {
-		return *user, nil
+		db.Database.Delete(&user)
+		sendData(rw, user, http.StatusOK)
 	}
 }
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"gorm/handlers"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -11,17 +10,20 @@ import (
 
 func main() {
 
+	//models.MigrarUser()
+
 	//Rutas
 	mux := mux.NewRouter()
 
-	//Responder al cliente
+	//EndPoind
 	mux.HandleFunc("/api/user/", handlers.GetUsers).Methods("GET")
 	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.GetUser).Methods("GET")
 	mux.HandleFunc("/api/user/", handlers.CreateUser).Methods("POST")
+
 	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.UpdateUser).Methods("PUT")
+
 	mux.HandleFunc("/api/user/{id:[0-9]+}", handlers.DeleteUser).Methods("DELETE")
 
-	//Servidor
-	fmt.Println("Run server: http://localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", mux))
+
 }
