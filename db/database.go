5c48e1fb5c7b1486db0c46aed160ee82cffe2efe
victor toledo <querynetworking@gmail.com>
2023-08-18 21:39:12 -0300

    creando conexion a bd

--- db/database.go
+++ db/database.go
@@ -1,87 +1,20 @@
 package db
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-//username:password@tcp(host:port)/database?charset=utf8
-const url = "root:123456@tcp(localhost:3306)/goweb_db"
-
-//Guarda la conexion
-var db *sql.DB
-
-//Realizar lac conexion
-func Connect() {
-	conection, err := sql.Open("mysql", url)
-	if err != nil {
+//Realiza la conexion
+var dsn = "root:123456@tcp(localhost:3306)/goweb_db?charset=utf8mb4&parseTime=True&loc=Local"
+var Database = func() (db *gorm.DB) {
+	if db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{}); err != nil {
+		fmt.Println("Error en la conexion", err)
 		panic(err)
+	} else {
+		fmt.Println("Conexion exitosa")
+		return db
 	}
-	fmt.Println("Conexion exitosa")
-	db = conection
-}
-
-//Cerrar la Conexion
-func Close() {
-	db.Close()
-}
-
-//Verificar la conexion
-func Ping() {
-	if err := db.Ping(); err != nil {
-		panic(err)
-	}
-}
-
-//Ferificar si una tabla existe o no
-func ExistsTable(tableName string) bool {
-	sql := fmt.Sprintf("SHOW TABLES LIKE '%s'", tableName)
-	rows, err := db.Query(sql)
-	if err != nil {
-		fmt.Println("Error: ", err)
-	}
-
-	return rows.Next()
-}
-
-//Crear una tabla en la base de datos
-func CreateTable(schema, name string) {
-
-	if !ExistsTable(name) {
-		_, err := db.Exec(schema)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-}
-
-//Eliminara Tabla
-func TruncateTable(tableName string) {
-	sql := fmt.Sprintf("TRUNCATE %s", tableName)
-	db.Exec(sql)
-}
-
-//Polimorfismo a Exec
-func Exec(query string, args ...interface{}) (sql.Result, error) {
-	Connect()
-	result, err := db.Exec(query, args...)
-	Close()
-	if err != nil {
-		fmt.Println(err)
-	}
-	return result, err
-}
-
-//Polimorfismo a Query
-func Query(query string, args ...interface{}) (*sql.Rows, error) {
-	Connect()
-	rows, err := db.Query(query, args...)
-	Close()
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return rows, nil
-}
+}()
